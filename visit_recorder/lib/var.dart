0740088253c3c7525a7e6416fa1117a708589e8b
rakinrkz <rakinuzzaman@gmail.com>
2024-08-01 18:18:06 +0600

    c1

---
+++ visit_recorder/lib/var.dart
@@ -0,0 +1,12 @@
+String place_name = ""; 
+
+
+  // List of items in dropdown menu
+  var location_dropdown_list = [
+    '- select -',
+    'Location 1',
+    'Location 2',
+    'Location 3',
+    'Location 4',
+    'Location 5',
+  ];
